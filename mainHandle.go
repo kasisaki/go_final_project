d14a91cd6666d6db4074dd5d2509bce4997a1ab6
mutaev <i.mutaev@gmail.com>
2024-05-02 01:16:13 +0300

    done step 3 parse repeat

--- mainHandle.go
+++ mainHandle.go
@@ -32,22 +32,12 @@ func handleTime(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte(s))
 }
 
-func handleMain(res http.ResponseWriter, req *http.Request) {
-	webDir := "./web/"
-	readFile, _ := os.ReadFile(webDir)
-
-	res.Write(readFile)
-}
-
 func NextDate(now time.Time, date string, repeat string) (newDate string, err error) {
 	parameterError := errors.New("wrong repeat parameter")
 	parameterSet := strings.Split(repeat, " ")
 	taskDate, err := time.Parse(DateLayout, date)
 	if err != nil {
 		return "", err
-	}
-	if now.After(taskDate) {
-		taskDate = now
 	}
 
 	lenP := len(parameterSet)
@@ -68,7 +58,11 @@ func NextDate(now time.Time, date string, repeat string) (newDate string, err er
 		if delay > 400 || delay < 1 {
 			return "", parameterError
 		}
-		return taskDate.AddDate(0, 0, delay).Format(DateLayout), nil
+		taskDate = taskDate.AddDate(0, 0, delay)
+		for taskDate.Before(now) {
+			taskDate = taskDate.AddDate(0, 0, delay)
+		}
+		return taskDate.Format(DateLayout), nil
 	// this case will add a year to the date
 	case "y":
 		if lenP != 1 {
@@ -113,6 +107,14 @@ func NextDate(now time.Time, date string, repeat string) (newDate string, err er
 			}
 			dates := make(map[time.Time]bool)
 			for _, day := range days {
+				if day < 0 {
+					y, m, _ := taskDate.Date()
+					_, _, dayX := time.Date(y, m+1, day+1, 0, 0, 0, 0, taskDate.Location()).Date()
+					//if dayX <= d {
+					//
+					//}
+					day = dayX
+				}
 				dates[nextMonthlyDate(taskDate, 0, day, false)] = true
 			}
 			return findEarliestDate(dates).Format(DateLayout), nil
@@ -126,6 +128,9 @@ func NextDate(now time.Time, date string, repeat string) (newDate string, err er
 				months[i], err = strconv.Atoi(str)
 				if err != nil {
 					return "", err
+				}
+				if months[i] > 12 {
+					return "", parameterError
 				}
 			}
 			for i, str := range strDays {
@@ -159,19 +164,20 @@ func nextMonthlyDate(t time.Time, month time.Month, day int, monthSpecific bool)
 		if m > month {
 			y++
 			m = month //return time.Date(y+1, month, d, 0, 0, 0, 0, t.Location())
-		}
-		if m == month {
+		} else if m == month {
 			if d < day {
 				m = month
 				// return time.Date(y, month, day, 0, 0, 0, 0, t.Location())
+			} else {
+				y++
+				// return time.Date(y+1, month, d, 0, 0, 0, 0, t.Location())
 			}
-			y++
-			// return time.Date(y+1, month, d, 0, 0, 0, 0, t.Location())
+		} else {
+			m = month
+			// return time.Date(y, month, day, 0, 0, 0, 0, t.Location())
 		}
-		m = month
-		// return time.Date(y, month, day, 0, 0, 0, 0, t.Location())
 	} else {
-		if d > day {
+		if d >= day {
 			m++
 			// return time.Date(y, m+1, day, 0, 0, 0, 0, t.Location())
 		}
